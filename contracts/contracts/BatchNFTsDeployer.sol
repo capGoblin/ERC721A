3c53416d172f00db6d862c760baf56237a2a9747
capGoblin <dharshan2457@gmail.com>
2024-01-20 11:43:13 +0530

    erc721A

---
+++ contracts/contracts/BatchNFTsDeployer.sol
@@ -0,0 +1,12 @@
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.13;
+
+// import "./BatchNFTs.sol";
+
+// contract BatchNFTsDeployer {
+//     address public deployedContract;
+
+//     constructor() {
+//         deployedContract = address(new BatchNFTs(block.timestamp, false));
+//     }
+// }
